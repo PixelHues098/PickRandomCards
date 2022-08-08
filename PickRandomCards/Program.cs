dcf4dd69793192ddaa0d758118121fe2aa8e88b5
PixelHues <102352684+PixelHues098@users.noreply.github.com>
2022-08-08 17:26:19 +0800

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PickRandomCards;
+
+
+    Console.Write("Enter the number of cards to pick: ");
+    string line = Console.ReadLine();
+    if (int.TryParse(line, out int numberofCards))
+    {
+        foreach (string card in CardPicker.PickSomeCards(numberofCards))
+        {
+            Console.WriteLine(card);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please Enter a Valid Number");
+    }
